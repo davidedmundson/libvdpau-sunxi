917561213ccdfe7df87d499b1d8c65737802358e
David Edmundson <kde@davidedmundson.co.uk>
2018-01-15 14:16:17 +0000

    rgb is for osd suckers

--- rgba.c
+++ rgba.c
@@ -55,34 +55,34 @@ VdpStatus rgba_create(rgba_surface_t *rgba,
 	rgba->height = height;
 	rgba->format = format;
 
-	if (device->osd_enabled)
-	{
-		rgba->data = cedrus_mem_alloc(device->cedrus, width * height * 4);
-		if (!rgba->data)
-			return VDP_STATUS_RESOURCES;
-
-// 		if(!device->g2d_enabled)
-// 			vdp_pixman_ref(rgba);
-
-		rgba->dirty.x0 = width;
-		rgba->dirty.y0 = height;
-		rgba->dirty.x1 = 0;
-		rgba->dirty.y1 = 0;
-		rgba_fill(rgba, NULL, 0x00000000);
-	}
+// 	if (device->osd_enabled)
+// 	{
+// 		rgba->data = cedrus_mem_alloc(device->cedrus, width * height * 4);
+// 		if (!rgba->data)
+// 			return VDP_STATUS_RESOURCES;
+//
+// // 		if(!device->g2d_enabled)
+// // 			vdp_pixman_ref(rgba);
+//
+// 		rgba->dirty.x0 = width;
+// 		rgba->dirty.y0 = height;
+// 		rgba->dirty.x1 = 0;
+// 		rgba->dirty.y1 = 0;
+// 		rgba_fill(rgba, NULL, 0x00000000);
+// 	}
 
 	return VDP_STATUS_OK;
 }
 
 void rgba_destroy(rgba_surface_t *rgba)
 {
-	if (rgba->device->osd_enabled)
-	{
+// 	if (rgba->device->osd_enabled)
+// 	{
 // 		if(!rgba->device->g2d_enabled)
 // 			vdp_pixman_unref(rgba);
 
-		cedrus_mem_free(rgba->data);
-	}
+// 		cedrus_mem_free(rgba->data);
+// 	}
 }
 
 VdpStatus rgba_put_bits_native(rgba_surface_t *rgba,
@@ -90,9 +90,9 @@ VdpStatus rgba_put_bits_native(rgba_surface_t *rgba,
                                uint32_t const *source_pitches,
                                VdpRect const *destination_rect)
 {
-	if (!rgba->device->osd_enabled)
+// 	if (!rgba->device->osd_enabled)
 		return VDP_STATUS_OK;
-
+/*
 	VdpRect d_rect = {0, 0, rgba->width, rgba->height};
 	if (destination_rect)
 		d_rect = *destination_rect;
@@ -118,7 +118,7 @@ VdpStatus rgba_put_bits_native(rgba_surface_t *rgba,
 
 	rgba->flags &= ~RGBA_FLAG_NEEDS_CLEAR;
 	rgba->flags |= RGBA_FLAG_DIRTY | RGBA_FLAG_NEEDS_FLUSH;
-	dirty_add_rect(&rgba->dirty, &d_rect);
+	dirty_add_rect(&rgba->dirty, &d_rect);*/
 
 	return VDP_STATUS_OK;
 }
@@ -134,9 +134,9 @@ VdpStatus rgba_put_bits_indexed(rgba_surface_t *rgba,
 	if (color_table_format != VDP_COLOR_TABLE_FORMAT_B8G8R8X8)
 		return VDP_STATUS_INVALID_COLOR_TABLE_FORMAT;
 
-	if (!rgba->device->osd_enabled)
+// 	if (!rgba->device->osd_enabled)
 		return VDP_STATUS_OK;
-
+/*
 	int x, y;
 	const uint32_t *colormap = color_table;
 	const uint8_t *src_ptr = source_data[0];
@@ -178,7 +178,7 @@ VdpStatus rgba_put_bits_indexed(rgba_surface_t *rgba,
 
 	rgba->flags &= ~RGBA_FLAG_NEEDS_CLEAR;
 	rgba->flags |= RGBA_FLAG_DIRTY | RGBA_FLAG_NEEDS_FLUSH;
-	dirty_add_rect(&rgba->dirty, &d_rect);
+	dirty_add_rect(&rgba->dirty, &d_rect);*/
 
 	return VDP_STATUS_OK;
 }
@@ -191,39 +191,39 @@ VdpStatus rgba_render_surface(rgba_surface_t *dest,
                               VdpOutputSurfaceRenderBlendState const *blend_state,
                               uint32_t flags)
 {
-	if (!dest->device->osd_enabled)
-		return VDP_STATUS_OK;
-
-	if (colors || flags)
-		VDPAU_DBG_ONCE("%s: colors and flags not implemented!", __func__);
-
-	// set up source/destination rects using defaults where required
-	VdpRect s_rect = {0, 0, 0, 0};
-	VdpRect d_rect = {0, 0, dest->width, dest->height};
-	s_rect.x1 = src ? src->width : 1;
-	s_rect.y1 = src ? src->height : 1;
-
-	if (source_rect)
-		s_rect = *source_rect;
-	if (destination_rect)
-		d_rect = *destination_rect;
-
-	// ignore zero-sized surfaces (also workaround for g2d driver bug)
-	if (s_rect.x0 == s_rect.x1 || s_rect.y0 == s_rect.y1 ||
-	    d_rect.x0 == d_rect.x1 || d_rect.y0 == d_rect.y1)
-		return VDP_STATUS_OK;
-
-	if ((dest->flags & RGBA_FLAG_NEEDS_CLEAR) && !dirty_in_rect(&dest->dirty, &d_rect))
-		rgba_clear(dest);
-
-	if (!src)
-		rgba_fill(dest, &d_rect, 0xffffffff);
-	else
-		rgba_blit(dest, &d_rect, src, &s_rect);
-
-	dest->flags &= ~RGBA_FLAG_NEEDS_CLEAR;
-	dest->flags |= RGBA_FLAG_DIRTY;
-	dirty_add_rect(&dest->dirty, &d_rect);
+// 	if (!dest->device->osd_enabled)
+// 		return VDP_STATUS_OK;
+//
+// 	if (colors || flags)
+// 		VDPAU_DBG_ONCE("%s: colors and flags not implemented!", __func__);
+//
+// 	// set up source/destination rects using defaults where required
+// 	VdpRect s_rect = {0, 0, 0, 0};
+// 	VdpRect d_rect = {0, 0, dest->width, dest->height};
+// 	s_rect.x1 = src ? src->width : 1;
+// 	s_rect.y1 = src ? src->height : 1;
+//
+// 	if (source_rect)
+// 		s_rect = *source_rect;
+// 	if (destination_rect)
+// 		d_rect = *destination_rect;
+//
+// 	// ignore zero-sized surfaces (also workaround for g2d driver bug)
+// 	if (s_rect.x0 == s_rect.x1 || s_rect.y0 == s_rect.y1 ||
+// 	    d_rect.x0 == d_rect.x1 || d_rect.y0 == d_rect.y1)
+// 		return VDP_STATUS_OK;
+//
+// 	if ((dest->flags & RGBA_FLAG_NEEDS_CLEAR) && !dirty_in_rect(&dest->dirty, &d_rect))
+// 		rgba_clear(dest);
+//
+// 	if (!src)
+// 		rgba_fill(dest, &d_rect, 0xffffffff);
+// 	else
+// 		rgba_blit(dest, &d_rect, src, &s_rect);
+//
+// 	dest->flags &= ~RGBA_FLAG_NEEDS_CLEAR;
+// 	dest->flags |= RGBA_FLAG_DIRTY;
+// 	dirty_add_rect(&dest->dirty, &d_rect);
 
 	return VDP_STATUS_OK;
 }
@@ -243,37 +243,37 @@ void rgba_clear(rgba_surface_t *rgba)
 
 void rgba_fill(rgba_surface_t *dest, const VdpRect *dest_rect, uint32_t color)
 {
-	if (dest->device->osd_enabled)
-	{
-		if(dest->device->g2d_enabled)
-		{
-// 			rgba_flush(dest);
-// 			g2d_fill(dest, dest_rect, color);
-		}
-		else
-		{
-			vdp_pixman_fill(dest, dest_rect, color);
-			dest->flags |= RGBA_FLAG_NEEDS_FLUSH;
-		}
-	}
+// 	if (dest->device->osd_enabled)
+// 	{
+// 		if(dest->device->g2d_enabled)
+// 		{
+// // 			rgba_flush(dest);
+// // 			g2d_fill(dest, dest_rect, color);
+// 		}
+// 		else
+// 		{
+// 			vdp_pixman_fill(dest, dest_rect, color);
+// 			dest->flags |= RGBA_FLAG_NEEDS_FLUSH;
+// 		}
+// 	}
 }
 
 void rgba_blit(rgba_surface_t *dest, const VdpRect *dest_rect, rgba_surface_t *src, const VdpRect *src_rect)
 {
-	if (dest->device->osd_enabled)
-	{
-		if(dest->device->g2d_enabled)
-		{
-// 			rgba_flush(dest);
-// 			rgba_flush(src);
-// 			g2d_blit(dest, dest_rect, src, src_rect);
-		}
-		else
-		{
-			vdp_pixman_blit(dest, dest_rect, src, src_rect);
-			dest->flags |= RGBA_FLAG_NEEDS_FLUSH;
-		}
-	}
+// 	if (dest->device->osd_enabled)
+// 	{
+// 		if(dest->device->g2d_enabled)
+// 		{
+// // 			rgba_flush(dest);
+// // 			rgba_flush(src);
+// // 			g2d_blit(dest, dest_rect, src, src_rect);
+// 		}
+// 		else
+// 		{
+// 			vdp_pixman_blit(dest, dest_rect, src, src_rect);
+// 			dest->flags |= RGBA_FLAG_NEEDS_FLUSH;
+// 		}
+// 	}
 }
 
 void rgba_flush(rgba_surface_t *rgba)
